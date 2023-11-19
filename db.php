c267b76c2580871473271a122898ef3b3a9b63df
s8140156 <s8140156@gmail.com>
2023-11-20 00:00:35 +0800

    function all()練習連結sql泛用指令寫法 學習資料表選擇泛用寫法

--- db.php
+++ db.php
@@ -6,10 +6,15 @@ dd($rows);
 
 
 function all($table=null,$where=''){
+	// 在all的函式增加$table參數
 	$dsn="mysql:host=localhost;charset=utf8;dbname=school";
 	$pdo=new PDO($dsn,'root','');
 
 	if(isset($table) && !empty($table)){
+		// 因為將$table參數給予null值(預設)(先佔位) 執行sql會造成fatal而無法執行
+		// 所以要寫判斷若有資料表而且不是空值就執行sql
+		// 若無 會有error訊息告知沒有指定資料表名稱 但這樣可以執行
+		// 在函式中增加參數,提升泛用性(用寬鬆的條件去使用, 只要放個參數、變數上去就大多數可適用)
 
 		if(is_array($where)){
 
@@ -38,5 +43,7 @@ function dd($array){
 	print_r($array);
 	echo "</pre>";
 }
+// dd direct dump 直接把資料印出來
+
 
 ?>
